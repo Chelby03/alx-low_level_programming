07b47028a10e43073e81e01e4ceb13d505986ae0
Chelsia <chelsiaroberto16@gmail.com>
2023-07-16 17:55:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- The function in which all c code goes under
+ * puts: writes a string to the standard output
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
