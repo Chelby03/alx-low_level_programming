c767bb0fb488da118552b93e8fe1cd35f356c23a
Chelsia <chelsiaroberto16@gmail.com>
2023-07-16 18:24:13 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point function
+ * printf: prints a text/string to the standard outpt
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
