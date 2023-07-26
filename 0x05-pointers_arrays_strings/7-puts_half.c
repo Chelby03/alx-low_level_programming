b27123df59f616e8951a39baf8b7e25db5844250
Chelsia <chelsiaroberto16@gmail.com>
2023-07-25 18:35:19 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the pointer pointing to a char
+ * Return: void
+ *
+ */
+
+void puts_half(char *str)
+{
+
+	int i, n;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	n = i / 2;
+	while (n <= i)
+	{
+		_putchar(str[n]);
+		n++;
+
+	}
+	_putchar('\n');
+}
