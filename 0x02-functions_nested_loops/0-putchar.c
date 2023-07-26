1426d8ef727dfc5129d494c6c575e77260ab6f37
Chelsia <chelsiaroberto16@gmail.com>
2023-07-26 12:32:44 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *main - Entrypoint for every function
+ *Return: 0
+ *Descriprion: prints a script using putchar
+ */
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int i, j, y, z;
+
+	for (i = 0; i < 3; i++)
+	{
+	
+		for (j = 0; j <= 9; j++)
+	{
+
+			 for (y = 0; y < 6; y++)
+	{
+				for (z = 0; z <= 9; z++)
+	{
+
+		_putchar(i);
+		_putchar(j);
+		_putchar(':');
+		_putchar(y);
+		_putchar(z);
+	}
+	
+	}
+	
+	}
+	}
+	_putchar('\n');
+}
